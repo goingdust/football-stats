0649c4a023294544746292faca5808bbb7a186d3
goingdust <dustinluck@gmail.com>
2022-03-14 17:14:26 -0400

    Build WinsAnalysis class

--- build/index.js
+++ build/index.js
@@ -2,17 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
 const csvFileReader = new CsvFileReader_1.CsvFileReader('db/football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWins = 0;
-matchReader.matches.forEach((match) => {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-});
 console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,4 @@ const csvFileReader = new CsvFileReader('db/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-matchReader.matches.forEach((match: MatchData) => {
-	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-		manUnitedWins++;
-	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-		manUnitedWins++;
-	}
-});
-
 console.log(`Man United won ${manUnitedWins} games`);
