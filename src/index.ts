ef41719f6ce6c2d627245d2356cf22483295a742
goingdust <dustinluck@gmail.com>
2022-03-14 17:47:52 -0400

    Remove now unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
 const matchReader = MatchReader.fromCsv('db/football.csv');
