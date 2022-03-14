4fbbb6c289edd028237dece9663d80021b5bd860
goingdust <dustinluck@gmail.com>
2022-03-14 17:35:58 -0400

    Build HtmlReport class and bring into index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('db/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Arsenal'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Arsenal'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
