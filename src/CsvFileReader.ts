ad773f7f4ae06f415330d8afe12edfffa9de8383
goingdust <dustinluck@gmail.com>
2022-03-14 12:00:37 -0400

    Create MatchData tuple to define each row type

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,10 @@ import * as fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-	data: string[][] = [];
+	data: MatchData[] = [];
 
 	constructor(public filename: string) {
 		this.read();
@@ -16,7 +18,7 @@ export class CsvFileReader {
 			})
 			.split('\n')
 			.map((row: string): string[] => row.split(','))
-			.map((row: string[]): any => {
+			.map((row: string[]): MatchData => {
 				return [
 					dateStringToDate(row[0]),
 					row[1],
