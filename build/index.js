a9f044fc115703450f968d618d5bc506bb720785
goingdust <dustinluck@gmail.com>
2022-03-14 11:08:29 -0400

    Create MatchResult enum to store all match result options

--- build/index.js
+++ build/index.js
@@ -30,12 +30,19 @@ const matches = fs
 })
     .split('\n')
     .map((row) => row.split(','));
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
 matches.forEach(match => {
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,19 @@ const matches = fs
 	.split('\n')
 	.map((row: string): string[] => row.split(','));
 
+// enum - enumeration
+enum MatchResult {
+	HomeWin = 'H',
+	AwayWin = 'A',
+	Draw = 'D',
+}
+
 let manUnitedWins = 0;
 
 matches.forEach(match => {
-	if (match[1] === 'Man United' && match[5] === 'H') {
+	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
 		manUnitedWins++;
-	} else if (match[2] === 'Man United' && match[5] === 'A') {
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
 		manUnitedWins++;
 	}
 });
